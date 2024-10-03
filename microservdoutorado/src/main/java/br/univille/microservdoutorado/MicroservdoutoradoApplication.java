f3017e9636d61ef4d1fc3b5dc2cf87feac2bcb8e
Felipe Serrano <104235448+FelipeSerranods@users.noreply.github.com>
2024-10-03 00:16:33 +0000

    first commit

---
+++ microservdoutorado/src/main/java/br/univille/microservdoutorado/MicroservdoutoradoApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microservdoutorado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservdoutoradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservdoutoradoApplication.class, args);
+	}
+
+}
---
+++ microservdoutorado/src/test/java/br/univille/microservdoutorado/MicroservdoutoradoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microservdoutorado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservdoutoradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
